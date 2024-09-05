5cd64346538fa60732022599b64108d439e3f646
LAAB <111896274+LAAB07@users.noreply.github.com>
2024-09-05 11:02:11 -0600

    Add some UI elements in the index file

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
+const body = document.body;
+
+const header = document.createElement("div");
+header.textContent = "Let's play some rock, paper, scissors game. The winner will be the one who gets the highest score after playing 5 rounds.";
+
+body.appendChild(header);
+
+const container = document.createElement("div");
+container.textContent = "Please select one of the following options:";
+
+body.appendChild(container);
+
+const btnRock = document.createElement("button");
+const btnPaper = document.createElement("button");
+const btnScissors = document.createElement("button");
+btnRock.textContent = "Rock";
+btnPaper.textContent = "Paper";
+btnScissors.textContent = "Scissors";
+
+container.appendChild(btnRock);
+container.appendChild(btnPaper);
+container.appendChild(btnScissors);
+
+const showOption = document.createElement("p");
+
+container.appendChild(showOption);
+
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach(button =>{
+    button.addEventListener("click", playGame);
+});
+
 /* This function calculates a random number with Math.random and use it to return rock, paper or scissors with if-else conditions */
 
 function getComputerChoice(){
@@ -18,35 +51,23 @@ function getComputerChoice(){
 
 /* This function gets the option from the user using the prompt method and apply some validations for the input in order to get a correct option */
 
-function getHumanChoice(){
+// function getHumanChoice(){
 
-    let userChoice;
+//     const userChoice = this.textContent;
+//     showOption.textContent = userChoice;
+//     // if(userChoice.tagName == "button")
+//     // {
+//     //     screen.textContent = userChoice;
+//     // }
+//     console.log(userChoice); 
+//     return userChoice;
 
-    userChoice = prompt("Please choice your option.\nRock? Paper? Scissors?\nYou can type UPPERCASE, lowercase or Capitalized Case");
-
-    if (userChoice === null || userChoice === "" || userChoice === undefined){
-            alert("You must enter a valid option into the prompt box!\nCannot be empty");
-            return getHumanChoice();
-    } else if (!/^[a-zA-Z]+$/.test(userChoice)){
-            alert("Please only use letters!\nYour input was: " + userChoice);
-            return getHumanChoice();
-    } else {
-            let userChoiceFix = userChoice.at(0).toUpperCase() + userChoice.slice(1).toLowerCase();
-            if (userChoiceFix === "Rock" || userChoiceFix === "Paper" || userChoiceFix === "Scissors"){
-                return userChoiceFix;
-            } else {
-                alert("You must enter a valid option into the prompt box!\nYour input was: " + userChoiceFix);
-                return getHumanChoice();    
-            }
-
-    }
-
-}
+// }
 
 /* Declaration of two variables that will be receiving the return values from getComputerChoice and getHumanChoice functions */ 
 
-let humanSelection = getHumanChoice();
-let computerSelection = getComputerChoice();
+// const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
 
 /* This function allows to play the game by 5 rounds, keeps track of the scores(human and computer) and declares a winner at the end or a draw if apply */
 
@@ -55,91 +76,132 @@ function playGame(humanChoice, computerChoice){
     let humanScore = 0;
     let computerScore = 0;
 
-    let round = 1;
+    const userChoice = this.textContent;
+    showOption.textContent = userChoice;
 
-    while(round<=5){
+    console.log(userChoice);
 
-        console.log("This is round number #" + round);
+    humanChoice = userChoice;
+
+    console.log("Please select your option");
+
+    // let round = 1;
+
+    // while(round<=5){
+
+        console.log("%cThis is round number #" + round, "color: blue");
+        const showRound = document.createElement("p");
+        const showWinner = document.createElement("p");
+        const showUser = document.createElement("p");
+        const showPC = document.createElement("p");
+        const showUserScore = document.createElement("p");
+        const showPcScore = document.createElement("p");
+        const showFinalWinner = document.createElement("p");
+        showRound.textContent = "This is round number: " + round;
+        container.appendChild(showRound);  
+        container.appendChild(showWinner);
+        container.appendChild(showUser);  
+        container.appendChild(showPC);
+        container.appendChild(showUserScore);  
+        container.appendChild(showPcScore);
+        container.appendChild(showFinalWinner);             
 
         if(humanChoice==="Rock" && computerChoice==="Scissors"){
-        console.log("You win! Rock beats Scissors");
+        console.log("%cYou win! Rock beats Scissors", "color: lime");
         console.log("");
+        showWinner.textContent = "You win! Rock beats Scissors";
         humanScore++;
         }
 
         else if(humanChoice==="Rock" && computerChoice==="Paper"){
-        console.log("You lose! Paper beats Rock");
+        console.log("%cYou lose! Paper beats Rock", "color: red");
         console.log("");
+        showWinner.textContent = "You lose! Paper beats Rock";
         computerScore++;
         }
 
         else if(humanChoice==="Paper" && computerChoice==="Rock"){
-        console.log("You Win! Paper beats Rock");
+        console.log("%cYou Win! Paper beats Rock", "color: lime");
         console.log("");
+        showWinner.textContent = "You Win! Paper beats Rock";
         humanScore++;
         }
 
         else if(humanChoice==="Paper" && computerChoice==="Scissors"){
-        console.log("You lose! Scissors beats Paper");
+        console.log("%cYou lose! Scissors beats Paper", "color: red");
         console.log("");
+        showWinner.textContent = "You lose! Scissors beats Paper";
         computerScore++;
         }
 
         else if(humanChoice==="Scissors" && computerChoice==="Paper"){
-        console.log("You Win! Scissors beats Paper");
+        console.log("%cYou Win! Scissors beats Paper", "color: lime");
         console.log("");
+        showWinner.textContent = "You Win! Scissors beats Paper";
         humanScore++;
         }
 
         else if(humanChoice==="Scissors" && computerChoice==="Rock"){
-        console.log("You lose! Rock beats Scissors");
+        console.log("%cYou lose! Rock beats Scissors", "color: red");
         console.log("");
+        showWinner.textContent = "You lose! Rock beats Scissors";
         computerScore++;
         }
 
         else{
         console.log("This is a draw! Nobody wins")
         console.log("");
+        showWinner.textContent = "This is a draw! Nobody wins";
         }
 
         console.log("Your choice was: " + humanChoice);
+        showUser.textContent = "Your choice was: " + humanChoice;
 
         console.log("The choice from computer was: " + computerChoice);
+        showPC.textContent = "The choice from computer was: " + computerChoice;
 
         console.log("");
     
         console.log("Your score is: " + humanScore);
+        showUserScore.textContent = "Your score is: " + humanScore;
     
         console.log("Score for computer is: " + computerScore);
+        showPcScore.textContent = "Score for computer is: " + computerScore;
 
         console.log("");
 
-        round++;
+    //     round++;
 
-        if(round===6){
+    //     if(round===6){
 
-            if(humanScore>computerScore){
-                console.log("Congratulations. You are the winner!")
-            } else if(humanScore<computerScore){
-                console.log("You lost. Plase try again.")
-            } else {
-                console.log("This is a draw. Try again.")
-            }
+    //         if(humanScore>computerScore){
+    //             console.log("%cCongratulations. You are the winner!", "color: lime");
+    //             showFinalWinner.textContent = "Congratulations. You are the winner!";
+    //         } else if(humanScore<computerScore){
+    //             console.log("%cYou lost. Please try again.", "color: red")
+    //             showFinalWinner.textContent = "You lost. Please try again.";
+    //         } else {
+    //             console.log("This is a draw. Try again.")
+    //             showFinalWinner.textContent = "This is a draw. Try again.";
+    //         }
 
-        } else {
-            humanChoice = getHumanChoice();
-            computerChoice = getComputerChoice();
-        }
+    //     } else {
+    //         humanChoice = getHumanChoice();
+    //         computerChoice = getComputerChoice();
+    //     }
 
-    }
+    // }
 
 }
 
-console.log("Lets play some rock, paper, scissors game. The winner will be the one who gets the highest score after playing 5 rounds");
-
-console.log("");
-
 playGame(humanSelection, computerSelection);
+
+
+
+
+
+
+
 
 
 
